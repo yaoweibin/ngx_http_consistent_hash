7d5e59a055cd934747e7bd7cfbaf6e4a33b943eb
Cedric Maion <cedric@maion.com>
2013-03-08 15:32:09 +0100

    Follow up (accessed => checked)

--- ngx_http_upstream_consistent_hash_module.c
+++ ngx_http_upstream_consistent_hash_module.c
@@ -401,7 +401,7 @@ ngx_http_upstream_get_consistent_hash_peer(ngx_peer_connection_t *pc,
                     break;
                 }
 
-                if (now - peer->accessed > peer->fail_timeout) {
+                if (now - peer->checked > peer->fail_timeout) {
                     peer->checked = now;
                     break;
                 }
